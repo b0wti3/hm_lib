1f246b167b2217938b090073d972e08600a370ff
Tie <32960848+b0wti3@users.noreply.github.com>
2017-12-29 17:27:34 -0500

    Splash / Desk Update
    Splash Update; desk.js development - mostly psudo code.

--- FrontEnd Scripts/athenaeum (main)/desk.js
+++ FrontEnd Scripts/athenaeum (main)/desk.js
@@ -1,11 +1,65 @@
 function(context, args)
 {
 	//desk.js - Part of the 'Library' Project; By: _meta_
-	//Purpose: Main library script. Used to: 
-	//  * Access top level script tools
-	//  * Access a user-offered service list
-	//  * See a collated account of the game's lore and user-made events. (History book of some kind)
+	// Main Access Script to the Athenaeum Library System
 
+    // DEV OUTLINE: What will this script do?
+	//  * Provides all users a read-only interface into the library's contents.
+    //  * Has a 'power-switch' built in to allow owner / admin to turn off the library during maintaince.
+    //  * Provides two output options: a "user-friendly" one (for new users), as well as a "bare-bones" view (experienced users).
+    //  * Does final text processing to prepare for viewing.
+    //  * Decides to send all library requests by the following categories:
+    //      1) Page Requests
+        //      A) Read / Search
+        //      B) Comment
+        //      C) Write / Edit
+        //      D) Delete / Create
+    //      2) Account Requests
+    //      3) Admin Requests
+    //      4) Store / Payment Requests
+    //      5) Help Requests
+    //      7) 
+    // DEV OUTLINE: What are some of the features built into this script?
+    //  * Contains a base 'page' to display content in. (In case backend scripts have to come down / when library is 'shutdown')
+
+
+
+    // Main Variable / Scriptor Setup
+    let lib = #fs.scripts.lib()
+
+    // Base Object / Base Page Array Setup
+    baseArr = [];
+    baseObj = {
+        // key-value pairs here.
+    };
+
+    // Core Functions
+
+
+
+    function pageBuilder (obj, type, decider) {
+        // This function is passed a object that has to remain a consistant standard and something that can easily be processed by multiple scripts. Should a function constructor be used? Or will simply defining one object be enough since the script only processes one page at a time?
+        
+        // obj: the obj that contains page information to be passed out to user.
+        // decider: A string set to either "full" or "slim" - decides whether to output a full page, or one that has only the bare essentials (decided by user).
+        
+        // takes object and picks correct default page frame to use from 'default_pages' script.
+        // formats default page based on user info, access level, and request(s).
+        // creates a string with final page compilation (and optional debug dump if requested by an admin / the owner? NYI / Thought Of)
+        // returns out completed string and debug dump.
+        
+    }
+    
+    // Code that filters out calls that could contain key values with scriptors / anything else unwanted:
+    /*
+    for (let key in args) {
+        if ( lib.is_obj(key) || !lib.is_str(key) ) {
+            // Reject Script Request and return out page message.
+        }
+    }
+    */
     const splash = #fs.athenaeum.lib_splash()
     return splash;
+    
+    
 }
--- FrontEnd Scripts/athenaeum (main)/lib_splash.js
+++ FrontEnd Scripts/athenaeum (main)/lib_splash.js
@@ -1,33 +1,30 @@
 function(context, args)
 {
     // Main Splash Image
-    let heaD = ["+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"];
-	let athenaeuM = [
+    let baseArr = [];
+    
+    let pageLine = "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    let mainPage = [
+        pageLine,
         "+                        WELCOME TO THE                       +",
         "+   ___  _____ __  __ _____ __  __  ___  _____ __ __ ___  __  +",
 		"+  ||=||  ||   ||==|| ||__  ||\\\\|| ||=|| ||__  || || || \\/ |  +",
-		"+  || ||  ||   ||  || ||___ || \\|| || || ||___ \\\\_\/\/ ||    |  +"
-    ].join("\n");
-    let librarY = [
+		"+  || ||  ||   ||  || ||___ || \\|| || || ||___ \\\\_\/\/ ||    |  +",
+        "+                                                             +",
         "+    	     __    __ ____  _____  ___  _____ _  _            +",
         "+    	     ||    || ||=)  ||_\/\/ ||=|| ||_\/\/ \\\\\/\/            +",
         "+    	     ||__| || ||_)) || \\\\ || || || \\\\  \/\/             +",
-        "+                                                             +"
-    ].join("\n");
-    let fooT = ["+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"];
-    let athenaeum1 = heaD.concat(athenaeuM,librarY,fooT).join("\n");
+        "+                                                             +",
+        pageLine
+    ];
+    
+    let pageMessage = [
+        "+  `eThis script is still under-construction. Try again later.`  +",
+        pageLine
+    ];
 
-    // Main Page Text
+    //Creation of Main Page on baseArr; Returns it to caller.
+    baseArr.push(mainPage,pageMessage);
 
-    let athenaeum2 = [
-        //"+  Welcome to the ATHENAUM LIBRARY!",
-        "+  `eThis script is still under-construction.` ",
-        "`eTry again later.`  +",
-        "\n",
-        "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-        ].join("");
-
-    //Creation of Main Page Text; Returns it to caller.
-    let athenaeum = athenaeum1 + "\n" + athenaeum2;
-    return athenaeum;
+    return baseArr;
 }
